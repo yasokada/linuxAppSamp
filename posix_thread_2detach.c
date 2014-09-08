cd24385061fe6849b8c009616169cb2f1bd174dd
Yasuhiko OKADA <yasokada@gmail.com>
2014-09-08 16:01:26 +0900

    add posix_thread_2detach.c

---
+++ posix_thread_2detach.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+/*
+http://blog.majide.com/2009/03/thread-sample-program/
+*/
+
+/*
+gcc [file] -lpthread
+*/
+
+void *print_message_function( void *ptr );
+
+int main(void)
+{
+	pthread_t thread1, thread2;
+	char *message1 = "Thread 1";
+	char *message2 = "Thread 2";
+	int iret1, iret2;
+
+	/* Create independent threads each of which will execute function */
+
+	iret1 = pthread_create( &thread1, NULL, print_message_function, (void*)message1);
+	iret2 = pthread_create( &thread2, NULL, print_message_function, (void*)message2);
+
+	sleep(1);
+}
+
+void *print_message_function( void *ptr )
+{
+	pthread_detach(pthread_self());
+
+	char *message;
+	message = (char *)ptr;
+	printf("%s \n", message);
+}
+
