f308f4be9315cce96f01c1272332aceea6976323
Yasuhiko OKADA <yasokada@gmail.com>
2014-09-09 14:36:18 +0900

    use mkdir(2) instead of mkdir shell command

--- sd_1mount.c
+++ sd_1mount.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 #include <stdbool.h> // for bool
 #include <sys/mount.h> // for mount(2)
+#include <sys/stat.h> // for mkdir(2)
 
 static bool sd_mount(void)
 {
+	const char *mountPos = "/media/card";
 	int ret;
-	system("mkdir /media/card"); // no check for return value
 
-	ret = mount("/dev/mmcblk0p1", "/media/card", "vfat",  MS_MGC_VAL | MS_SYNCHRONOUS, NULL);
-//	ret = mount("/dev/sdf1", "/media/card", "vfat", MS_MGC_VAL | MS_SYNCHRONOUS, NULL);
+	ret = mkdir(mountPos, 0777); 
+	// no check for mkdir
+
+	ret = mount("/dev/mmcblk0p1", mountPos, "vfat",  MS_MGC_VAL | MS_SYNCHRONOUS, NULL);
 	if (ret == 0) {
 		return true;
 	} else {
