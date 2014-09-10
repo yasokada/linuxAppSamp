4f43ffb2f8bed07560f05d32f3f9da06fda5244a
Yasuhiko OKADA <yasokada@gmail.com>
2014-09-10 17:25:58 +0900

    use only tty as serial input

--- sio_input.c
+++ sio_input.c
@@ -6,51 +6,41 @@
 
 int main(int argc, char **argv)
 {
-    struct termios ttyio;
-    struct termios stdio;
-    struct termios old_stdio;
+    struct termios tty_io;
+    struct termios preSet;
     int tty_fd;
 
-    unsigned char c='D';
-    tcgetattr(STDOUT_FILENO,&old_stdio);
+    unsigned char c=0;
+    tcgetattr(STDOUT_FILENO, &preSet);
 
-    printf("Please start with %s /dev/ttyS1 (for example)\n",argv[0]);
-    memset(&stdio,0,sizeof(stdio));
-    stdio.c_iflag=0;
-    stdio.c_oflag=0;
-    stdio.c_cflag=0;
-    stdio.c_lflag=0;
-    stdio.c_cc[VMIN]=1;
-    stdio.c_cc[VTIME]=0;
-    tcsetattr(STDOUT_FILENO,TCSANOW,&stdio);
-    tcsetattr(STDOUT_FILENO,TCSAFLUSH,&stdio);
-    fcntl(STDIN_FILENO, F_SETFL, O_NONBLOCK);       // make the reads non-blocking
+    if (argc < 2) {
+        printf("[cmd] [device of serial tty]\n");
+        printf("e.g. %s /dev/ttyPS0\n", argv[0]);
+        return;
+    }
 
-    memset(&ttyio,0,sizeof(ttyio));
-    ttyio.c_iflag=0;
-    ttyio.c_oflag=0;
-    ttyio.c_cflag=CS8|CREAD|CLOCAL;           // 8n1, see termios.h for more informattyion
-    ttyio.c_lflag=0;
-    ttyio.c_cc[VMIN]=1;
-    ttyio.c_cc[VTIME]=5;
+    memset(&tty_io, 0, sizeof(tty_io));
+    tty_io.c_iflag=0;
+    tty_io.c_oflag=0;
+    tty_io.c_cflag=CS8 | CREAD | CLOCAL;           // 8n1
+    tty_io.c_lflag=0;
+    tty_io.c_cc[VMIN]=1;
+    tty_io.c_cc[VTIME]=5;
 
     tty_fd=open(argv[1], O_RDWR | O_NONBLOCK);      
-    cfsetospeed(&ttyio,B115200);            // 115200 baud
-    cfsetispeed(&ttyio,B115200);            // 115200 baud
+    cfsetospeed(&tty_io,B115200);            // 115200 baud
+    cfsetispeed(&tty_io,B115200);            // 115200 baud
 
-    tcsetattr(tty_fd,TCSANOW,&ttyio);
+    tcsetattr(tty_fd, TCSANOW, &tty_io);
     while (c!='q')
     {
-        if (read(tty_fd,&c,1)>0) {
-            write(STDOUT_FILENO,&c,1);  // on the serial port
+        if (read(tty_fd, &c, 1)>0) {
+            write(STDOUT_FILENO, &c, 1);
         } 
-        if (read(STDIN_FILENO,&c,1)>0) {
-            write(tty_fd,&c,1);  // on the console
-        }
     }
 
     close(tty_fd);
-    tcsetattr(STDOUT_FILENO,TCSANOW,&old_stdio);
+    tcsetattr(STDOUT_FILENO, TCSANOW, &preSet);
 
     return 0;
 }
