42c4cbb19cf3554d6683521290274974660e53ee
Yasuhiko OKADA <yasokada@gmail.com>
2014-09-11 16:58:58 +0900

    rearrange comments and refactor

--- ipc-sharedMemory.c
+++ ipc-sharedMemory.c
@@ -1,26 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include <sys/wait.h>
-#include <sys/shm.h>
+#include <sys/shm.h> // for shared memory
 #include <stdbool.h>
 
 /*
 ipc: inter process communication
+*/
 
-sample of
-- fork()
-- shared memory
+/*
+[result]
+start child 1
+proc 1 
+-----
+sample string
+-----
+proc 2 
+main > fin
 */
 
 static int s_segId; // segment id
 
-static bool proc_one(void)
+static void proc_one(void)
 {
     char *paddr;
-
-    sleep(1);
 
     if (fork() == 0) {
         printf("start child 1\n");
@@ -28,70 +31,64 @@ static bool proc_one(void)
         // attach
         if ((paddr = shmat(s_segId, NULL, 0)) == (void *)-1) {
             perror("child1 > shmat failed");
-            return false;
+            exit(EXIT_FAILURE);
         }
 
+        // set
         strcpy(paddr, "sample string\n");
+
         // detach
         if (shmdt(paddr) == -1) {
             perror("child1 > shmdt failed");
-            return false;
+            exit(EXIT_FAILURE);
         }
 
         printf("proc 1 \n");
-        return true;
+        exit(EXIT_SUCCESS);
     }
-    return false;
 }
 
-static bool proc_two(void)
+static void proc_two(void)
 {
     char *paddr;
 
-    sleep(1);
-
     if (fork() == 0) {
+        sleep(1);
+
         // attach
         if ((paddr = shmat(s_segId, NULL, SHM_RDONLY)) == (void *)-1) {
             perror("child2 > shmat failed");
-            return false;
+            exit(EXIT_FAILURE);
         }
 
         printf("-----\n");
+        // get
         printf("%s", paddr);
         printf("-----\n");
 
         // detach
         if (shmdt(paddr) == -1) {
             perror("child2 > shmdt failed");
-            return false;
+            exit(EXIT_FAILURE);
         }
 
         printf("proc 2 \n");
-        return true;
+        exit(EXIT_SUCCESS);
     }
-    return false;
-}
+} 
 
 int main(void)
 {
     const int kSize = 100;
 
     if ( (s_segId = shmget(IPC_PRIVATE, kSize, IPC_CREAT | 0666)) 
-         == -1){
+         == -1 ){
         perror("main > shmget failed");
         exit(EXIT_FAILURE);
     }
 
-    if (proc_one() == false) {
-        exit(EXIT_FAILURE);
-        return;
-    }
-
-    if (proc_two() == false) {
-        exit(EXIT_FAILURE);
-        return;
-    }
+    proc_one();
+    proc_two();
 
     int loop;
     for (loop=0; loop < 2; loop++) {
@@ -105,7 +102,5 @@ int main(void)
 
     printf("main > fin\n");
 
-    // fflush(stdout);
-    // fflush(stdout);
     return EXIT_SUCCESS;
 }
