005b326553fa9776fe0cdac807e606109ccbd8d9
Yasuhiko OKADA <yasokada@gmail.com>
2014-09-11 09:45:02 +0900

    refactor

--- sio_input.c
+++ sio_input.c
@@ -9,9 +9,7 @@ int main(int argc, char **argv)
     struct termios tty_io;
     struct termios preSet;
     int tty_fd;
-
-    unsigned char c=0;
-    tcgetattr(STDOUT_FILENO, &preSet);
+    unsigned char code = 0;
 
     if (argc < 2) {
         printf("[cmd] [device of serial tty]\n");
@@ -19,24 +17,26 @@ int main(int argc, char **argv)
         return;
     }
 
-    memset(&tty_io, 0, sizeof(tty_io));
-    tty_io.c_iflag=0;
-    tty_io.c_oflag=0;
-    tty_io.c_cflag=CS8 | CREAD | CLOCAL;           // 8n1
-    tty_io.c_lflag=0;
-    tty_io.c_cc[VMIN]=1;
-    tty_io.c_cc[VTIME]=5;
+    tcgetattr(STDOUT_FILENO, &preSet);
 
-    tty_fd=open(argv[1], O_RDWR | O_NONBLOCK);      
-    cfsetospeed(&tty_io,B115200);            // 115200 baud
-    cfsetispeed(&tty_io,B115200);            // 115200 baud
+    memset(&tty_io, 0, sizeof(tty_io));
+    tty_io.c_iflag = 0;
+    tty_io.c_oflag = 0;
+    tty_io.c_cflag = CS8 | CREAD | CLOCAL;           // 8n1
+    tty_io.c_lflag = 0;
+    tty_io.c_cc[VMIN] = 1; // 1 character
+    tty_io.c_cc[VTIME] = 5; // 0.5 sec
+
+    tty_fd = open(argv[1], O_RDWR | O_NONBLOCK);      
+    cfsetospeed(&tty_io, B115200);            // 115200 baud
+    cfsetispeed(&tty_io, B115200);            // 115200 baud
 
     tcsetattr(tty_fd, TCSANOW, &tty_io);
-    while (c!='q')
+    while (code != 'q')
     {
-        if (read(tty_fd, &c, 1)>0) {
-            write(STDOUT_FILENO, &c, 1);
-        } 
+        if (read(tty_fd, &code, 1)>0) {
+            write(STDOUT_FILENO, &code, 1);
+        }
     }
 
     close(tty_fd);
