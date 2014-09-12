0f49149f18a4d49a58e7ce53a9175fdc3fa79814
Yasuhiko OKADA <yasokada@gmail.com>
2014-09-12 10:43:40 +0900

    sample of open and close

---
+++ mqueue_0open_close.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <mqueue.h> // for message queue
+#include <sys/stat.h>
+#include <stdlib.h> // for EXIT_FAILURE
+
+/*
+gcc [file] -lrt
+*/
+
+int main()
+{
+	mqd_t mqd;
+	int flags;
+	int ret;
+
+	flags = O_RDWR | O_CREAT;
+
+	// POSIX IPC name should start with "/"
+	mqd = mq_open("/mq", flags,
+		(S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH) );
+	if (mqd < 0) {
+		printf("open failed\n");
+		exit(EXIT_FAILURE);
+	}
+	printf("open ok\n");
+
+	sleep(1);
+
+	ret = mq_close(mqd);
+	if (ret != 0) {
+		printf("open failed\n");
+		exit(EXIT_FAILURE);		
+	}
+	printf("close ok\n");
+
+	return 0;
+}
