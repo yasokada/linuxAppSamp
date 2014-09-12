d6406f0aace16509287a51b730569c3b74d4053a
Yasuhiko OKADA <yasokada@gmail.com>
2014-09-12 13:32:04 +0900

    refactor > tab deleted

--- mqueue_2setMsgSize.c
+++ mqueue_2setMsgSize.c
@@ -1,65 +1,65 @@
-	#include <stdio.h>
-	#include <mqueue.h> // for message queue
-	#include <sys/stat.h>
-	#include <stdlib.h> // for EXIT_FAILURE
-	#include <string.h>
-    #include <errno.h>
+#include <stdio.h>
+#include <mqueue.h> // for message queue
+#include <sys/stat.h>
+#include <stdlib.h> // for EXIT_FAILURE
+#include <string.h>
+#include <errno.h>
 
-	/*
-	gcc [file] -lrt
-	*/
+/*
+gcc [file] -lrt
+*/
 
-	static void showAttr(mqd_t mqd)
-	{
-		struct mq_attr attr;
+static void showAttr(mqd_t mqd)
+{
+	struct mq_attr attr;
 
-		mq_getattr(mqd, &attr);
+	mq_getattr(mqd, &attr);
 
-		printf("maxmsg = %d\n", attr.mq_maxmsg);
-		printf("msgsize = %d\n", attr.mq_msgsize);
-		printf("curmsgs = %d\n", attr.mq_curmsgs);
+	printf("maxmsg = %d\n", attr.mq_maxmsg);
+	printf("msgsize = %d\n", attr.mq_msgsize);
+	printf("curmsgs = %d\n", attr.mq_curmsgs);
 
+}
+
+int main()
+{
+	mqd_t mqd;
+	int flags;
+	int ret;
+	struct mq_attr attr;
+
+	flags = O_RDWR | O_CREAT;
+
+	mq_unlink("/mq");
+
+	// mq_maxmsg should be less than and equal to the value defined
+	// in /proc/sys/fs/mqueue/msg_max
+	attr.mq_flags = 0;
+	attr.mq_maxmsg = 8;
+	attr.mq_msgsize = 1024;
+	attr.mq_curmsgs = 0;
+
+	mqd = mq_open("/mq", flags,
+		(S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH),
+		&attr );
+
+	if (mqd < 0) {
+		printf("open failed %d\n", mqd);
+		exit(EXIT_FAILURE);
 	}
+	printf("open ok\n");
 
-	int main()
-	{
-		mqd_t mqd;
-		int flags;
-		int ret;
-		struct mq_attr attr;
+	sleep(1);
 
-		flags = O_RDWR | O_CREAT;
-
-		mq_unlink("/mq");
-
-		// mq_maxmsg should be less than and equal to the value defined
-		// in /proc/sys/fs/mqueue/msg_max
-		attr.mq_flags = 0;
-		attr.mq_maxmsg = 8;
-		attr.mq_msgsize = 1024;
-		attr.mq_curmsgs = 0;
-
-		mqd = mq_open("/mq", flags,
-			(S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH),
-			&attr );
-
-		if (mqd < 0) {
-			printf("open failed %d\n", mqd);
-			exit(EXIT_FAILURE);
-		}
-		printf("open ok\n");
-
-		sleep(1);
-
-		showAttr(mqd);
+	showAttr(mqd);
 
 
-		ret = mq_close(mqd);
-		if (ret != 0) {
-			printf("open failed\n");
-			exit(EXIT_FAILURE);		
-		}
-		printf("close ok\n");
-
-		return 0;
+	ret = mq_close(mqd);
+	if (ret != 0) {
+		printf("open failed\n");
+		exit(EXIT_FAILURE);		
 	}
+	printf("close ok\n");
+
+	return 0;
+}
