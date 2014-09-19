bb8004d256289f89df0b58cb5ada46a9ae08c98f
Yasuhiko OKADA <yasokada@gmail.com>
2014-09-19 14:46:45 +0900

    add while() to avoid spurious wakeups

--- mutex_signal.c
+++ mutex_signal.c
@@ -13,12 +13,15 @@ main > unlocked
 thread > unlocked
 */
 
+int condition = 0;
+
 pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
 
 void *threadproc(void *data) {
 	pthread_mutex_lock(&lock);
 	printf("thread > locked\n");
+	condition = 1;
 	pthread_cond_signal(&cond);
 	printf("thread > signal\n");
 	pthread_mutex_unlock(&lock);
@@ -32,8 +35,10 @@ int main(int argc, char *argv[]) {
 	pthread_create(&thread, NULL, &threadproc, NULL);
 	printf("main > created\n");
 
-	pthread_cond_wait(&cond, &lock);
-	printf("main > signaled\n");
+	while(!condition) { // while: to avoid spurious wakeups
+		pthread_cond_wait(&cond, &lock);
+		printf("main > signaled\n");
+	}
 
 	pthread_mutex_unlock(&lock);
 	printf("main > unlocked\n");
