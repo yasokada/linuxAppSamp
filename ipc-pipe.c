c9d2f2fcceeb9df1aadfbbce55e3972ed1515e59
Yasuhiko OKADA <yasokada@gmail.com>
2014-10-02 10:39:12 +0900

    sample of fork and pipe

---
+++ ipc-pipe.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+static void funcWrite(const char *msg, int cnt, FILE *stream)
+{
+	int loop;
+
+	for(loop=0; loop<cnt; loop++) {
+		fprintf(stream, "%s\n", msg);
+		fflush(stream);
+		sleep(1);
+	}
+}
+
+static void funcRead(FILE *stream)
+{
+	char zbuf[512];
+
+	while( !feof(stream) && !ferror(stream)
+	&& fgets(zbuf, sizeof(zbuf), stream) != NULL ) {
+		printf("%s", zbuf);
+	    fflush(stdout);
+	}
+}
+
+int main(void)
+{
+	int fds[2];
+	pid_t pid;
+	FILE *stream;
+
+	printf("start\n");
+
+	pipe(fds);
+
+	pid = fork(); /***/
+	printf("after fork\n");
+
+	if (pid == (pid_t) 0) { // parent
+		close(fds[1]);
+		stream = fdopen(fds[0], "r");
+		funcRead(stream);
+		close(fds[0]);
+	} else { // child
+		close(fds[0]);
+		stream = fdopen(fds[1], "w");
+		funcWrite("message from child", 3, stream);
+		close(fds[1]);
+	}
+
+	return 0;
+}
