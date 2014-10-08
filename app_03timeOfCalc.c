63c949dfb0f3bced1e9612af3b52e522cdbd33e7
Yasuhiko OKADA <yasokada@gmail.com>
2014-10-08 09:26:53 +0900

    correct functions to be times including return fval

--- app_03timeOfCalc.c
+++ app_03timeOfCalc.c
@@ -4,7 +4,7 @@
 
 // -lrt
 
-const long kLoopNum = 100000000L;
+const long kLoopNum = 10000;
 
 static int disp_precision(void)
 {
@@ -19,59 +19,63 @@ static int disp_precision(void)
 	return 0;
 }
 
-static void funcToBeTimed_floatAdd(float fadd)
+static float funcToBeTimed_floatAdd(void)
 {
-	volatile long idx;
+	int idx;
 	float fval = 0.0f;
 
 	for(idx=0; idx<kLoopNum; idx++) {
-		fval = fval + fadd;
+		fval = fval + 1.001f;
 	}
+	return fval;
 }
 
-static void funcToBeTimed_floatMulti(float fmul)
+static float funcToBeTimed_floatMulti(void)
 {
-	volatile long idx;
+	int idx;
 	float fval = 3.14f;
 
 	for(idx=0; idx<kLoopNum; idx++) {
-		fval = fval * fmul;
+		fval = fval * 1.001f;
 	}
+	return fval;
 }
 
-static void funcToBeTimed_floatDiv(float fdiv)
+static float funcToBeTimed_floatDiv(void)
 {
-	volatile long idx;
+	int idx;
 	float fval = 3141590000000.00f;
 
 	for(idx=0; idx<kLoopNum; idx++) {
-		fval = fval / fdiv;
+		fval = fval / 1.001f;
 	}
+	return fval;
 }
 
 static void disp_elapsed(int procNo)
 {
 	struct timespec tp1, tp2;
 	long dsec, dnsec;
+	float fval;
 
 	/***/
 	switch(procNo) {
 	case 0:
 		printf("add\n");
 		clock_gettime(CLOCK_REALTIME, &tp1);
-		funcToBeTimed_floatAdd(3.14f);
+		fval = funcToBeTimed_floatAdd();
 		clock_gettime(CLOCK_REALTIME, &tp2);
 		break;
 	case 1:
 		printf("multi\n");
 		clock_gettime(CLOCK_REALTIME, &tp1);
-		funcToBeTimed_floatMulti(1.001f);
+		fval = funcToBeTimed_floatMulti();
 		clock_gettime(CLOCK_REALTIME, &tp2);
 		break;
 	default:
 		printf("div\n");
 		clock_gettime(CLOCK_REALTIME, &tp1);
-		funcToBeTimed_floatDiv(1.00001f);
+		fval = funcToBeTimed_floatDiv();
 		clock_gettime(CLOCK_REALTIME, &tp2);
 		break;
 	}
