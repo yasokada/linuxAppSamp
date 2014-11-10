2b4ae2ce006ef21d70c8f86e9ffa90a22ce61f3f
Yasuhiko OKADA <yasokada@gmail.com>
2014-11-10 15:39:53 +0900

    debug recvfrom/sendto

--- app_04udp_server.c
+++ app_04udp_server.c
@@ -1,37 +1,52 @@
 #include <stdio.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
+#include <string.h>
+#include <stdlib.h>
 #include <arpa/inet.h>
-#include <netdb.h>
-#include <string.h> // for memset()
+#include <sys/socket.h>
 
 #define SIZE_BUF 200
 
-int main(void) {
+int main(void)
+{
     unsigned short port = 9880;
-    int recvSocket;
     struct sockaddr_in rcvAddr;
+    int sockfd, slen, rcvdLen;
+    char rcvBuf[SIZE_BUF];
     int ret;
-    int rcvdLen;
-    char szBuf[SIZE_BUF];
 
-    memset(&rcvAddr, 0, sizeof(rcvAddr));
-    rcvAddr.sin_port = htons(port);
+    slen = sizeof(rcvAddr);
+
+    sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    if (sockfd == -1) {
+        exit(EXIT_FAILURE);
+    }
+    memset((char *) &rcvAddr, 0, sizeof(rcvAddr));     
     rcvAddr.sin_family = AF_INET;
+    rcvAddr.sin_port = htons(port);
     rcvAddr.sin_addr.s_addr = htonl(INADDR_ANY);
-
-    recvSocket = socket(AF_INET, SOCK_DGRAM, 0);
-
-    ret = bind(recvSocket, (struct sockaddr *) &rcvAddr, sizeof(rcvAddr));
+    bind(sockfd , (struct sockaddr*)&rcvAddr, sizeof(rcvAddr) );
 
     while(1) {
-        rcvdLen = recvfrom(recvSocket, szBuf, SIZE_BUF, 0, NULL, NULL);
+        fflush(stdout);
+        rcvdLen = recvfrom(sockfd, rcvBuf, SIZE_BUF, 0, 
+            (struct sockaddr *) &rcvAddr, &slen);
         if (rcvdLen == -1) {
-            ret = close(recvSocket);
-            break;
+            close(sockfd);
+            exit(EXIT_FAILURE);
         }
-        printf("rx: %s\n", szBuf);
-    }
-}
+        if (strstr(rcvBuf, "req") == NULL) {    // accept only "req"
+            continue;
+        }
+        printf("rx: %s", rcvBuf);
 
+        // TODO: remove <LF>
+
+        ret = sendto(sockfd, rcvBuf, rcvdLen, 0, (struct sockaddr*) &rcvAddr, slen);         
+        if (ret == -1) {
+            close(sockfd);
+            exit(EXIT_FAILURE);
+        }
+    }
+    close(sockfd);
+    return 0;
+}
