709c892aecd49b589a650435ad3d2fe3392f265d
Yasuhiko OKADA <yasokada@gmail.com>
2014-11-13 14:50:56 +0900

    1st commit

---
+++ socket_tcp_nonblk_client.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <string.h> // for memset()
+
+int main(int argc, char **argv) {
+    int loop;
+    char destIP[80];
+    unsigned short port = 9880;
+    int destSocket;
+    struct sockaddr_in destAddr;
+    char *toSendText = "test message";
+
+    if (argc < 2) {
+        printf("[cmd] [dest IP addr]\n");
+        printf("e.g. ./a.out 127.0.0.1\n");
+        return;
+    }
+
+    strcpy(destIP, argv[1]);
+    printf("to %s\n", destIP);
+
+    memset(&destAddr, 0, sizeof(destAddr));
+    destAddr.sin_addr.s_addr = inet_addr(destIP);
+    destAddr.sin_port = htons(port);
+    destAddr.sin_family = AF_INET;
+
+    destSocket = socket(AF_INET, SOCK_STREAM, 0);
+
+    connect(destSocket, (struct sockaddr *) &destAddr, sizeof(destAddr));
+
+    for(loop=0; loop<3; loop++) {
+        printf("tx:%s\n", toSendText);
+        send(destSocket, toSendText, strlen(toSendText)+1, 0);
+        usleep(100000);
+//        sleep(1);
+    }
+    
+    close(destSocket);
+}
+
