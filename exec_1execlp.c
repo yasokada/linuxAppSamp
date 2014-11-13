e8e9f34887613cca2e08b5848cf18f8a85d3ce86
Yasuhiko OKADA <yasokada@gmail.com>
2014-11-13 09:17:37 +0900

    add DBG statement

--- exec_1execlp.c
+++ exec_1execlp.c
@@ -1,8 +1,11 @@
 #include <unistd.h>
+#include <stdio.h>
 
 int main(int argc, char *argv)
 {
 	execlp("ls", "ls", "-l", NULL);
+
+	printf("DBG > 7\n"); // does not come here unless error
 
 	return 0;
 }
